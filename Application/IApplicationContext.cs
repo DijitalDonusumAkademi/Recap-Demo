f85523c0cd3871df5ddf755b75bb6d414632ae9e
Zeki SERT <zeki.sert@yahoo.com>
2020-10-28 21:57:09 +0300

    Application Layer IApplicationContext.cs Eklendi.

---
+++ Application/IApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application
+{
+    public interface IApplicationContext
+    {
+        DbSet<Product> Products { get; set; }
+        Task<int> SaveChanges();
+    }
+}
+
+//Bu interface bizim ürünlerimizin kalıcı olarak saklanması, getirilmesi gibi işlemleri yapacak olan sınıflar için bir sözleşmedir bunun implementasyonu Infrastructure katmanında yapılacaktır. Bunu bir interface yaparak Application ve Infrastructure katmanı arasındaki bağımlılığı azaltıyorum.
