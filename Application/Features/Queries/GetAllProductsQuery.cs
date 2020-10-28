0cd5c12f38ee13d2962903a8043ad3563c7a22e0
Zeki SERT <zeki.sert@yahoo.com>
2020-10-28 22:03:12 +0300

    Application Layer GetAllProductsQuery.cs Eklendi.

---
+++ Application/Features/Queries/GetAllProductsQuery.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Queries
+{
+    public class GetAllProductsQuery : IFeatureCommand
+    {
+        public class GetAllProductsQueryHandler
+        {
+            private readonly IApplicationContext _context;
+
+            public GetAllProductsQueryHandler(IApplicationContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<IEnumerable<Product>> Handle(GetAllProductsQuery query)
+            {
+                var productList = await _context.Products.ToListAsync();
+                return productList?.AsReadOnly();
+            }
+        }
+        
+    }
+}
