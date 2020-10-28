5577ec8a4b0398f8229b58145ee7a5d51da584d6
Zeki SERT <zeki.sert@yahoo.com>
2020-10-28 22:26:40 +0300

    Infrastructure Layer AoolicationContext.cs

---
+++ Infrastructure/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class ApplicationContext
+    {
+        public class ApplicationContext : DbContext, IApplicationContext
+        {
+            public ApplicationContext(DbContextOptions options) : base(options)
+            {
+            }
+
+            public DbSet<Product> Products { get; set; }
+            public async Task<int> SaveChanges()
+            {
+                return await base.SaveChangesAsync();
+            }
+        }
+    }
+}
